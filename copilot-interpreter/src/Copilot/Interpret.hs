a1face88a55d33aaa6b399d789fb28b4ed0e9a62
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-06-09 15:10:49 +0000

    WIP: Drawing streams.
    [ci skip]

--- copilot-interpreter/src/Copilot/Interpret.hs
+++ copilot-interpreter/src/Copilot/Interpret.hs
@@ -7,8 +7,10 @@
 module Copilot.Interpret
   ( Format (..)
   , interpret
+  , paint
   ) where
 
+import Data.List
 import Copilot.Core
 import Copilot.Interpret.Eval
 import Copilot.Interpret.Render
@@ -23,7 +25,64 @@ interpret :: Format  -- ^ Format to be used for the output.
           -> String
 interpret format k spec =
   case format of
-    Table -> renderAsTable e
-    CSV   -> renderAsCSV e
+    Table   -> renderAsTable e
+    CSV     -> renderAsCSV e
   where
     e = eval Haskell k spec
+
+paint :: Int     -- ^ Number of steps to interpret.
+      -> Spec    -- ^ Specification to interpret.
+      -> IO ()
+paint k spec =
+    paintEvaluation e
+  where
+    e = eval Haskell k spec
+
+paintEvaluation :: ExecTrace
+                -> IO ()
+paintEvaluation e = do
+    putStrLn "\\documentclass{standalone}"
+    putStrLn "\\usepackage{tikz}"
+    putStrLn "\\usepackage{tikz-timing}"
+    putStrLn "\\begin{document}"
+    putStrLn "\\begin{tikztimingtable}"
+    printTriggerOutputs
+    putStrLn "\\end{tikztimingtable}"
+    putStrLn "\\end{document}"
+    printObserverOutputs
+  where
+
+    printTriggerOutputs = mapM_ (printTriggerOutput) trigs
+
+    printTriggerOutput (name, ls) = putStrLn $ name ++ " & "
+                                                 ++ concatMap printTriggerOutputListElem ls
+                                                 ++ "\\\\"
+
+    printTriggerOutputListElem Nothing  = ""
+    printTriggerOutputListElem (Just x) = showValues x
+
+    showValues [] = ""
+    showValues ls@(x:_)
+      | isBoolean x = showValuesBoolean ls
+      | otherwise   = showValuesNumber  ls
+
+    showValuesBoolean ls = concatMap showValuesBooleanSegment $ group ls
+
+    showValuesBooleanSegment ls = show (length ls) ++ highOrLow (head ls)
+      where
+        highOrLow "true"  = "H"
+        highOrLow "false" = "L"
+
+    showValuesNumber ls = concatMap showValuesNumberSegment $ group ls
+
+    showValuesNumberSegment ls =
+       show (length ls) ++ "D{" ++ (head ls) ++ "}"
+
+    printObserverOutputs = return ()
+
+    trigs = interpTriggers e
+    obsvs = interpObservers e
+
+isBoolean "true" = True
+isBoolean "false" = True
+isBoolean _ = False
